95a2c074ab4cec3e74d4aa88e1bc92275c5f9304
socket772 <socket772@protonmail.com>
2025-02-11 15:16:31 +0100

    Reso modulare il programma

--- install.sh
+++ install.sh
@@ -1,122 +1,157 @@
 #!/usr/bin/bash
-set -e
 # $1 = block device disk
 # $2 = stage 3 file path
-
-options=("disk" "stage" "base-setup" "base-chroot" "kernel" "system" "tools" "bootloader" "exit")
-
-installer_path="$(realpath $0)"
-stage_path="$(realpath $2)"
-
+set -e
 
 if [[ $UID != "0" ]]; then
     echo "usa 'sudo -i'"
     exit
 fi
 
+options=("disk" "stage" "base-setup" "base-chroot" "kernel" "system" "tools" "bootloader" "exit")
+
+installer_path="$(realpath $0)"
+stage_path="$(realpath $2)"
+
+disk_step() {
+    fdisk "$1"
+    mkfs.fat -F 32 "$1"1
+    mkfs.ext4 -L "gentoo-root" "$1"2
+    mkdir --parents /mnt/gentoo
+    mount "$1"2 /mnt/gentoo
+    mkdir --parents /mnt/gentoo/efi
+    cp "$installer_path" /mnt/gentoo
+}
+
+stage_step() {
+    cd /mnt/gentoo
+    chronyd -q
+    tar xpvf "$stage_path" --xattrs-include='*.*' --numeric-owner -C "/mnt/gentoo"
+    echo '# Compiler flags to set for all languages' >> /mnt/gentoo/etc/portage/make.conf
+    echo 'COMMON_FLAGS="-march=native -O2 -pipe"' >> /mnt/gentoo/etc/portage/make.conf
+    echo '# Use the same settings for both variables' >> /mnt/gentoo/etc/portage/make.conf
+    echo 'CFLAGS="${COMMON_FLAGS}"' >> /mnt/gentoo/etc/portage/make.conf
+    echo 'CXXFLAGS="${COMMON_FLAGS}"' >> /mnt/gentoo/etc/portage/make.conf
+    echo 'ACCEPT_LICENSE="*"' >> /mnt/gentoo/etc/portage/make.conf
+}
+
+base_setup_step() {
+    cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+    mount --types proc /proc /mnt/gentoo/proc
+    mount --rbind /sys /mnt/gentoo/sys
+    mount --make-rslave /mnt/gentoo/sys
+    mount --rbind /dev /mnt/gentoo/dev
+    mount --make-rslave /mnt/gentoo/dev
+    mount --bind /run /mnt/gentoo/run
+    mount --make-slave /mnt/gentoo/run
+    chroot /mnt/gentoo
+}
+
+base_chroot_step() {
+    mount /dev/sda1 /efi
+    emerge-webrsync
+    emerge --sync
+    eselect news read
+    eselect profile list
+    read -p "Enter correct list element: " listnameoption
+    eselect profile set "$listnameoption"
+    ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    eselect locale list
+    read -p "Enter correct list element: " localeoption
+    eselect locale set "$localeoption"
+    env-update
+}
+
+kernel_step() {
+    emerge sys-kernel/linux-firmware
+    echo 'sys-apps/systemd boot' >> /etc/portage/package.use/systemd-boot
+    echo 'sys-kernel/installkernel systemd-boot' >> /etc/portage/package.use/systemd-boot
+    echo "sys-kernel/installkernel dracut" > /etc/portage/package.use/installkernel
+    echo "quiet splash" > /etc/kernel/cmdline
+    emerge sys-apps/systemd sys-kernel/installkernel
+    emerge sys-kernel/gentoo-kernel
+    echo 'USE="dist-kernel"' >> /etc/portage/make.conf
+    emerge @module-rebuild
+    emerge sys-kernel/gentoo-sources
+    eselect kernel list
+    read -p "Enter correct list element: " listkerneloption
+    eselect kernel set "$listkerneloption"
+}
+
+system() {
+    blkid
+    echo '/dev/sda1 /efi vfat umask=0077 0 2' >> /etc/fstab
+    echo '/dev/sda2 / ext4 defaults,noatime 0 1' >> /etc/fstab
+    read -p "Enter hostname: " hostameinput
+    echo "$hostameinput" > /etc/hostname
+    emerge net-misc/dhcpcd
+    echo "127.0.0.1 $hostameinput.homenetwork $hostameinput localhost" > /etc/hosts
+    echo "Metti la password di root"
+    passwd
+    systemd-machine-id-setup
+    systemd-firstboot --prompt
+    systemctl enable dhcpcd
+    systemctl preset-all --preset-mode=enable-only
+    systemctl preset-all
+}
+
+tools_step() {
+    emerge app-shells/bash-completion
+    systemctl enable systemd-timesyncd.service
+    emerge sys-fs/dosfstools
+    emerge net-misc/dhcpcd
+}
+
+bootloader_step() {
+    emerge sys-apps/systemd
+    bootctl install
+    bootctl list
+    echo "Verifica se esiste l'entrata, esegui 'emerge --ask --config sys-kernel/gentoo-kernel-bin' e poi ricontrolla"
+    echo "esegui 'systemctl enable dhcpd.service --now' per internet"
+    echo "Riavvia il pc"
+}
+
 select menu in "${options[@]}";
 do
     echo -e "\nyou picked $menu ($REPLY)"
+    # Disks - 1
     if [[ $menu == "disk" ]]; then
-        # Disks
-        fdisk "$1"
-        mkfs.fat -F 32 "$1"1
-        mkfs.ext4 -L "gentoo-root" "$1"2
-        mkdir --parents /mnt/gentoo
-        mount "$1"2 /mnt/gentoo
-        mkdir --parents /mnt/gentoo/efi
-        cp "$installer_path" /mnt/gentoo
+        disk_step
     fi
+    # Stage - 2
     if [[ $menu == "stage" ]]; then
-        # Stage
-        cd /mnt/gentoo
-        chronyd -q
-        tar xpvf "$stage_path" --xattrs-include='*.*' --numeric-owner -C "/mnt/gentoo"
-        echo '# Compiler flags to set for all languages' >> /mnt/gentoo/etc/portage/make.conf
-        echo 'COMMON_FLAGS="-march=native -O2 -pipe"' >> /mnt/gentoo/etc/portage/make.conf
-        echo '# Use the same settings for both variables' >> /mnt/gentoo/etc/portage/make.conf
-        echo 'CFLAGS="${COMMON_FLAGS}"' >> /mnt/gentoo/etc/portage/make.conf
-        echo 'CXXFLAGS="${COMMON_FLAGS}"' >> /mnt/gentoo/etc/portage/make.conf
-        echo 'ACCEPT_LICENSE="*"' >> /mnt/gentoo/etc/portage/make.conf
+        stage_step
     fi
+    # Base - 3
     if [[ $menu == "base-setup" ]]; then
-        # Base
-        cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-        mount --types proc /proc /mnt/gentoo/proc
-        mount --rbind /sys /mnt/gentoo/sys
-        mount --make-rslave /mnt/gentoo/sys
-        mount --rbind /dev /mnt/gentoo/dev
-        mount --make-rslave /mnt/gentoo/dev
-        mount --bind /run /mnt/gentoo/run
-        mount --make-slave /mnt/gentoo/run
-        chroot /mnt/gentoo /bin/bash
+        base_setup_step
     fi
+    # Base chroot - 4
     if [[ $menu == "base-chroot" ]]; then
-        # Base chroot
-        mount /dev/sda1 /efi
-        emerge-webrsync
-        emerge --sync
-        eselect news read
-        eselect profile list
-        read -p "Enter correct list element: " listnameoption
-        eselect profile set "$listnameoption"
-        ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
-        echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-        locale-gen
-        eselect locale list
-        read -p "Enter correct list element: " localeoption
-        eselect locale set "$localeoption"
-        env-update
+        base_chroot_step
     fi
+    # Kernel - 5
     if [[ $menu == "kernel" ]]; then
-        # Kernel
-        emerge sys-kernel/linux-firmware
-        echo 'sys-apps/systemd boot' >> /etc/portage/package.use/systemd-boot
-        echo 'sys-kernel/installkernel systemd-boot' >> /etc/portage/package.use/systemd-boot
-        echo "sys-kernel/installkernel dracut" > /etc/portage/package.use/installkernel
-        echo "quiet splash" > /etc/kernel/cmdline
-        emerge sys-apps/systemd sys-kernel/installkernel
-        emerge sys-kernel/gentoo-kernel-bin
-        echo 'USE="dist-kernel"' >> /etc/portage/make.conf
-        emerge @module-rebuild
-        emerge sys-kernel/gentoo-sources
-        eselect kernel list
-        read -p "Enter correct list element: " listkerneloption
-        eselect kernel set "$listkerneloption"
+        kernel_step
     fi
+    # System - 6
     if [[ $menu == "system" ]]; then
-        # System
-        blkid
-        echo '/dev/sda1 /efi vfat umask=0077 0 2' >> /etc/fstab
-        echo '/dev/sda2 / ext4 defaults,noatime 0 1' >> /etc/fstab
-        read -p "Enter hostname: " hostameinput
-        echo "$hostameinput" > /etc/hostname
-        emerge net-misc/dhcpcd
-        systemctl enable dhcpcd
-        echo "127.0.0.1 $hostameinput.homenetwork $hostameinput localhost" > /etc/hosts
-        echo "Metti la password di root"
-        passwd
-        systemd-machine-id-setup
-        systemd-firstboot --prompt
-        systemctl preset-all --preset-mode=enable-only
-        systemctl preset-all
+        system
     fi
+    # Tools - 7
     if [[ $menu == "tools" ]]; then
-        # Tools
-        emerge app-shells/bash-completion
-        systemctl enable systemd-timesyncd.service
-        emerge sys-fs/dosfstools
-        emerge net-misc/dhcpcd
+        tools_step
     fi
+    # Bootloader - 8
     if [[ $menu == "bootloader" ]]; then
-        # Bootloader
-        emerge sys-apps/systemd
-        bootctl install
-        bootctl list
-        read -p "Verifica se esiste l'entrata, esegui 'emerge --ask --config sys-kernel/gentoo-kernel-bin' e poi ricontrolla"
-        echo "Riavvia il pc"
+        bootloader_step
     fi
+    #9
     if [[ $menu == "exit" ]]; then
         exit
     fi
 done
+
+
